a179900ba2afb22699d6c98f6a428cf1173c48b0
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-11 17:10:59 +0500

    Refactor PopularityEngine.py to update weights and calculate popularity score

--- PopularityEngine.py
+++ PopularityEngine.py
@@ -8,3 +8,15 @@ class PopularityRecommender():
         self.alpha = 0.5
         self.beta = 1
 
+    def UpdateWeights(self, a, b):
+        self.alpha = a
+        self.beta = b
+    
+    def CalculatePopularity(self):
+
+        def CalculatePopularityScore(row):
+            a = (self.alpha * row['Popularity Index'])*(self.beta * row['Avg Visitors'])
+            a /= np.mean(self.dataset['Popularity Index'])
+            return a
+        
+        self.dataset['Popularity'] = self.dataset.apply(CalculatePopularityScore, axis=1)
