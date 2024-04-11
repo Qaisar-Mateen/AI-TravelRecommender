f76b75106788a3606f6f2b40ad926a88a394f5db
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-11 17:31:15 +0500

    Refactor PopularityRecommender.py to include alpha and beta parameters in the constructor

--- PopularityRecommender.py
+++ PopularityRecommender.py
@@ -3,26 +3,26 @@ import numpy as np
 
 
 class PopularityRecommender():
-    def __init__(self, dataset='world-popularity.csv'):
+    def __init__(self, dataset='world-popularity.csv', alpha=0.5, beta=1):
         self.dataset = pd.read_csv(dataset)
-        self.alpha = 0.5
-        self.beta = 1
+        self.alpha = alpha
+        self.beta = beta
 
     def UpdateWeights(self, a, b):
         self.alpha = a
         self.beta = b
     
     def CalculatePopularity(self):
+        """
+            Popularity Formula:
+            Popularity = ((alpha * Popularity Index) * (beta * Avg Visitors) / mean(Popularity Index))/ Max Popularity
+        """
 
         def CalculatePopularityScore(row):
 
-            """
-            Popularity Formula:
-            Popularity = ((alpha * Popularity Index) * (beta * Avg Visitors) / mean(Popularity Index))/ Max Popularity
-            """
-
             a = (self.alpha * row['Popularity Index'])*(self.beta * row['Avg Visitors'])
             a /= np.mean(self.dataset['Popularity Index'])
+
             return a
         
         self.dataset['Popularity'] = self.dataset.apply(CalculatePopularityScore, axis=1)
