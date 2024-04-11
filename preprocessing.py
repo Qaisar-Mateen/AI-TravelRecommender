82730c47817cf33f1d3a3765aae29ad5b0f0ecfb
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-11 17:05:41 +0500

    Refactor preprocessing.py for data cleaning and column manipulation

---
+++ preprocessing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import random
+
+
+data = pd.read_csv('world-cities.csv')
+df = pd.read_csv('world-countries.csv')
+# print(data)
+
+# Add a new column with random values
+# data["avg temp"] = [random.randint(75, 150) for _ in range(len(data))]
+
+# df.drop('avg cost per day', axis=1, inplace=True)
+
+# df.drop('keyword', axis=1, inplace=True)
+
+
+
+#a = data.groupby('country')['avg cost per day'].mean().astype(int)
+#df = df.set_index('country').join(a).reset_index()
+#df['avg cost per day'] = df['avg cost per day'].fillna(0).astype(int)
+
