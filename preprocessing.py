ac94a21222752bcb9551d7254be5f246b3e08a6a
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-11 17:07:55 +0500

    Refactor preprocessing.py to include visitor count for countries

--- preprocessing.py
+++ preprocessing.py
@@ -111,3 +111,186 @@ def calculate_popularity(country):
         return random.uniform(18.23422, 37.05334)
 
 i = 0
+
+def calculate_visitors(country):
+    global i
+    if country == 'Mexico':
+        print(i)
+        i+=1
+        return int(51128000)
+    if country == 'United States':
+        print(i)
+        i+=1
+        return int(45037000)
+    if country == 'Thailand':
+        print(i)
+        i+=1
+        return int(39916000)
+    if country == 'Italy':
+        print(i)
+        i+=1
+        return int(38419000)
+    if country == 'Pakistan':
+        print(i)
+        i+=1
+        return int(966_000)
+    if country == 'Spain':
+        print(i)
+        i+=1
+        return int(36410000)
+    if country == 'Canada':
+        print(i)
+        i+=1
+        return int(32430000)
+    if country == 'Hungary':
+        print(i)
+        i+=1
+        return int(31641000)
+    if country == 'China':
+        print(i)
+        i+=1
+        return int(30402000)
+    if country == 'Saudi Arabia':
+        print(i)
+        i+=1
+        return int(20292000)
+    if country == 'India':
+        print(i)
+        i+=1
+        return int(17914000)
+    if country == 'Turkey':
+        print(i)
+        i+=1
+        return int(15_971_000)
+    if country == 'Denmark':
+        
+        print(i)
+        i+=1
+        return int(15_000_000)
+    if country == 'Greece':
+        print(i)
+        i+=1
+        return int(14_710_000)
+    if country == 'Austria':
+        print(i)
+        i+=1
+        return int(12_730_000)
+    if country == 'Germany':
+        print(i)
+        i+=1
+        return int(11_690_000)
+    if country == 'United Arab Emirates':
+        print(i)
+        i+=1
+        return int(11_480_000)
+    if country == 'South Korea':
+        print(i)
+        i+=1
+        return int(17_500_000)
+    if country == 'Vietnam':
+        print(i)
+        i+=1
+        return int(18_000_000)
+    if country == 'Netherlands':
+        print(i)
+        i+=1
+        return int(20_100_000)
+    if country == 'Japan':
+        print(i)
+        i+=1
+        return int(32_300_000)
+    if country == 'Australia':
+        print(i)
+        i+=1
+        return int(9_300_000)
+    if country == 'Brazil':
+        print(i)
+        i+=1
+        return int(6_600_000)
+    if country == 'Egypt':
+        print(i)
+        i+=1
+        return int(11_300_000)
+    if country == 'Malaysia':
+        print(i)
+        i+=1
+        return int(26_100_000)
+    if country == 'New Zealand':
+        print(i)
+        i+=1
+        return int(4_500_000)
+    if country == 'Portugal':
+        print(i)
+        i+=1
+        return int(12_800_000)
+    if country == 'Russia':
+        print(i)
+        i+=1
+        return int(24_600_000)
+    if country == 'Singapore':
+        print(i)
+        i+=1
+        return int(14_400_000)
+    if country == 'South Africa':
+        print(i)
+        i+=1
+        return int(10_200_000)
+    if country == 'Sweden':
+        print(i)
+        i+=1
+        return int(7_300_000)
+    if country == 'Argentina':
+        print(i)
+        i+=1
+        return int(6_800_000)
+    if country == 'Chile':
+        print(i)
+        i+=1
+        return int(5_700_000)
+    if country == 'Colombia':
+        print(i)
+        i+=1
+        return int(4_900_000)
+    if country == 'Indonesia':
+        print(i)
+        i+=1
+        return int(16_100_000)
+    if country == 'Ireland':
+        print(i)
+        i+=1
+        return int(10_800_000)
+    if country == 'Morocco':
+        print(i)
+        i+=1
+        return int(12_300_000)
+    if country == 'Nepal':
+        print(i)
+        i+=1
+        return int(601_360)
+    if country == 'Switzerland':
+        print(i)
+        i+=1
+        return int(9_500_000)
+    if country == 'Taiwan':
+        print(i)
+        i+=1
+        return int(11_600_000)
+    if country == 'Ukraine':
+        print(i)
+        i+=1
+        return int(14_700_000)
+    if country == 'Bangladesh':
+        print(i)
+        i+=1
+        return int(3_000_000)
+    if country == 'France':
+        print(i)
+        i+=1
+        return int(117_109_000)
+    if country == 'Poland':
+        print(i)
+        i+=1
+        return int(88_515_000)
+    else:
+        return random.randrange(100_000, 2_000_000)
+
