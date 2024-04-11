1fb6a7fc2f84a740c121ef935af4a327a680e402
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-11 17:27:46 +0500

    Refactor PopularityRecommender.py to remove unused imports and update popularity calculation

--- PopularityRecommender.py
+++ PopularityRecommender.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class PopularityRecommender():
-    def __init__(self, dataset):
+    def __init__(self, dataset='world-popularity.csv'):
         self.dataset = pd.read_csv(dataset)
         self.alpha = 0.5
         self.beta = 1
