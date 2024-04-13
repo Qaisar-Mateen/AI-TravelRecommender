79e1463e45a03628efa781003001232b1d7910ee
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-13 16:01:32 +0500

    Refactor ContentRecommender.py to include TF-IDF vectorization and cosine similarity calculation

--- ContentRecommender.py
+++ ContentRecommender.py
@@ -90,3 +90,12 @@
 #     print(recommend2)
 
 # recomender(country, budget)
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from time import sleep as s
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
