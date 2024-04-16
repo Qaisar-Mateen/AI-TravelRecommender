af39683d2afcfe56ae6fd76102da8d0195763e98
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-16 15:40:21 +0500

    Fix missing_countries calculation and update world-countries.csv in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -317,12 +317,17 @@ a = pd.read_csv('world-popularity.csv')
 b = pd.read_csv('world-countries.csv')
 
 
+f=0
+for i in range(len(b)):
+    for j in range(len(a)):
+        if b['Country'][i] == a['Country'][j]:
+            f+=1  
+            print(f,b['Country'][i], a['Country'][j], 'at index', i, j)
+            #a['ID'].iloc[j] = b['ID'].iloc[i]
+        
+
+print(a[a['ID'] == -1, 'Country'])
 print(a)
 print(b)
 
-# b.to_csv('world-countries.csv', index=True)
-
-# print(b[a['Country'] != b['country']])
-
-# print(a[a[('id','Country')] == b[('id','country')]])
-
+a.to_csv('world-popularity.csv', index=False)
