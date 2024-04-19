92c4caec6a11fd1605152776c24648473faad756
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-19 18:09:13 +0500

    Refactor CollaborativeRecommender.py to remove unused code and improve code readability

--- ColaborativeRecommender.py
+++ ColaborativeRecommender.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import tez
 
-class CollaborativeRecommender:
-    def __init__(self, data):
-        self.data = data
+# class CollaborativeRecommender:
+#     def __init__(self, data):
+#         self.data = data
     
