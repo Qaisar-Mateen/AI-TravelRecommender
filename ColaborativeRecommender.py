c6776c9053b33c975583e79afe17734930a8ef90
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-19 18:29:56 +0500

    Refactor CollaborativeRecommender.py to add RecommenderModel class and implement forward and moniterMetrics methods

--- ColaborativeRecommender.py
+++ ColaborativeRecommender.py
@@ -11,6 +11,29 @@ import torch.nn as nn
 
 train_df, test_df = None, None
 
+class RecommenderModel(tez.Model):
+    def __init__(self, num_users, num_country):
+        super().__init__()
+
+        self.user_embed = nn.Embedding(num_users, 32)
+        self.country_embed = nn.Embedding(num_country, 32)
+        self.out = nn.Linear(64, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def moniterMetrics(self, outputs, targets):
+        outputs = outputs.cpu().detach().numpy()
+        targets = targets.cpu().detach().numpy()
+        return {'rmse': np.sqrt(((outputs - targets) ** 2).mean())}
+
+    def forward(self, user, country, rating):
+        user = self.user_embed(user)
+        country = self.country_embed(country)
+        out = torch.cat([user, country], 1)
+        out = self.out(out)
+        out = self.sigmoid(out)
+        loss = nn.MSELoss()(out, rating.view(-1, 1))
+        return out, loss
+
 def train():
     global train_df, test_df
     df = pd.DataFrame('ratings.csv')
