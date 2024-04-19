dd2620d3cca06356c2043a0a70acb1204dbad6c6
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-19 18:21:33 +0500

    Refactor CollaborativeRecommender.py to add train function and split data for training and testing

--- ColaborativeRecommender.py
+++ ColaborativeRecommender.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import numpy as np
 import tez
+from sklearn import model_selection
+import torch
+import torch.nn as nn
 
 # class CollaborativeRecommender:
 #     def __init__(self, data):
 #         self.data = data
-    
+
+train_df, test_df = None, None
+
+def train():
+    global train_df, test_df
+    df = pd.DataFrame('ratings.csv')
+
+    train_df, test_df = model_selection.train_test_split(df, test_size=0.2, random_state=42, stratify=df['rating'])
