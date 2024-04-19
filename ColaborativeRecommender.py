0b96c2e3127899b17cc20c90553d58e0b00dc6e9
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-19 18:40:52 +0500

    Refactor CollaborativeRecommender.py to add learning rate scheduler and preprocessing for user and country labels

--- ColaborativeRecommender.py
+++ ColaborativeRecommender.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import tez
-from sklearn import model_selection
+from sklearn import model_selection, preprocessing
 import torch
 import torch.nn as nn
 
@@ -19,6 +19,7 @@ class RecommenderModel(tez.Model):
         self.country_embed = nn.Embedding(num_country, 32)
         self.out = nn.Linear(64, 1)
         self.sigmoid = nn.Sigmoid()
+        self.step_scheduler_after = 'epoch'
 
     def moniterMetrics(self, outputs, targets):
         outputs = outputs.cpu().detach().numpy()
@@ -31,11 +32,34 @@ class RecommenderModel(tez.Model):
         out = torch.cat([user, country], 1)
         out = self.out(out)
         out = self.sigmoid(out)
+
         loss = nn.MSELoss()(out, rating.view(-1, 1))
-        return out, loss
+        metrics = self.moniterMetrics(out, rating.view(-1, 1))
+
+        return out, loss, metrics
+    
+    def fetchOptimizer(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+    
+    def fetchScheduler(self):
+        return torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.1)
+
 
 def train():
     global train_df, test_df
     df = pd.DataFrame('ratings.csv')
 
+    lbl_user = preprocessing.LabelEncoder()
+    lbl_country = preprocessing.LabelEncoder()
+
+    df['user'] = lbl_user.fit_transform(df['user'])
+    df['country'] = lbl_country.fit_transform(df['country'])
+
     train_df, test_df = model_selection.train_test_split(df, test_size=0.2, random_state=42, stratify=df['rating'])
+
+    model = RecommenderModel(num_users=df['user'].nunique(), num_country=df['country'].nunique())
+
+    model.fit(train_df, test_df, train_bs=1024, valid_bs=1024, fp16=True)
+
+    if __name__ == "__main__":
+        train()
